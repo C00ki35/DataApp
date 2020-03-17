faa893be0b92f9911f35e16de840a715e47dc667
Cooki3 <pgcdigit@gmail.com>
2020-03-17 13:32:07 +0000

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <SearchBar setSearchTerm={this.setSearchTerm} />
+        <Search setSearchTerm={this.setSearchTerm} />
         <Chart />
         <Title />
         <EventList searchTerm={this.state.searchTerm} />
